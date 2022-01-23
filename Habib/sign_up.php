07d4a4cf6bccbdce5306998a7bebc41efd85f60d
Habib-eng <jlejla66@gmail.com>
2022-01-23 17:06:55 +0100

    third_commit

--- Habib/sign_up.php
+++ Habib/sign_up.php
@@ -1,5 +1,5 @@
 <?php
-function is_empty($list){
+/*function is_empty($list){
     $result = false;
     foreach ($list as $value){
         if (empty($value)){
@@ -8,13 +8,13 @@ function is_empty($list){
         }
     }
     return $result;
-}
+}*/
 require 'db.php' ;
 $query = "INSERT INTO `authors` (`firstName`,`lastName`,`email`,`password`,`level`) VALUES (?,?,?,?,?) ";
 $stmt = $db->prepare($query);
-if (isset($_POST["save"])){
+if (isset($_POST["save"])&&){
     $data = [$_POST['firstName'],$_POST['lastName'],$_POST['email'],$_POST['password'],(int)$_POST['level']];
     $stmt->execute($data);
 }
-//require 'blog.php'; 
+require 'blog.php'; 
 ?>
